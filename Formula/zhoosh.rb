1f75cf4b8d5d1927e10ab441892cb0df85060556
Tianna Lopes <63357829+TiannaLopes@users.noreply.github.com>
2024-10-16 21:57:30 -0400

    Update Zhoosh to v1.0.1

--- Formula/zhoosh.rb
+++ Formula/zhoosh.rb
@@ -1,8 +1,8 @@
 class Zhoosh < Formula
   desc "A terminal styling package for Zsh"
   homepage "https://github.com/TiannaLopes/zhoosh"
-  url "https://github.com/TiannaLopes/zhoosh/archive/v1.0.0.tar.gz"
-  sha256 "the_sha256_hash_of_the_tarball"
+  url "https://github.com/TiannaLopes/zhoosh/archive/v1.0.3.tar.gz"
+  sha256 "YOUR_SHA256_HASH_HERE"
 
   def install
     bin.install "install.sh"
